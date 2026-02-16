e3f1ecefb82d52f8b9bf99112c378492ebedfa8c
danrick <dcode0516@gmail.com>
2026-02-16 22:51:55 +0800

    enhance display of barangay certificate

--- resources/views/pdf/barangay_clearance.blade.php
+++ resources/views/pdf/barangay_clearance.blade.php
@@ -31,46 +31,104 @@
     <img style="width: 120%; margin-top: -50px; margin-left: -50px;" src="{{ asset('public/barangay/header/ubaldo.png') }}" alt="Barangay Header">
     @endif
 
+</div> 
+
+ <!-- <div style="margin-left:90px; margin-top: -3px;" >
+     <img style="width: 5%; height:50%;" src="{{ asset('public/barangay/header/line.jpg') }}" alt="Barangay Header">
+</div> -->
+<!-- LEFT SIDE OFFICIALS -->
+ <!-- Vertical Line Separator -->
+<div style="position:absolute; left:195px; top:80px; width:400px; height:1990px;">
+      <img style="width: 5%; height:50%;" src="{{ asset('public/barangay/header/line.jpg') }}" alt="Barangay Header">
 </div>
 
- 
- <div style="margin-left:160px; margin-right:15px;">
+<div style="position:absolute; left:-20px; margin-top: -0px;  font-family:'Bookman Old Style', serif; font-size:8pt; line-height:1.4;">
+
+    <p style="font-weight:bold;">SANGGUNIANG BARANGAY</p>
+    
+
+    <p style="font-weight:bold;">ANTONIO P. FLORES</p>
+    <p>Barangay Chairman</p>
+
+    <br>
+    <p style="font-weight:bold;">BARANGAY KAGAWAD</p>
+
+    <p><strong>MARGARET D. CASAS</strong><br>
+    Livelihood & PWD</p>
+
+    <p><strong>ALAN M. BADELLES</strong><br>
+    Senior Citizen & Agriculture</p>
+
+    <p><strong>JEROMEL R. AREOLA</strong><br>
+    BDRR, Finance & Infrastructure</p>
+
+    <p><strong>ROBERT V. CAPANGPANGAN</strong><br>
+    Transportation, Communication, Peace & Order</p>
+
+    <p><strong>WILMA P. ALSONADO</strong><br>
+    Health & Sanitation, Social Services</p>
+
+    <p><strong>FERLINA C. OTASA</strong><br>
+    Women’s, Education & Environment</p>
+
+    <p><strong>SANTOS M. BINERBA</strong></p>
+
+    <p><strong>JHON DELL D. TABANIA</strong><br>
+    Sports</p>
+
+    <br>
+
+    <p><strong>RHIENNA STEPHANIE A. CATIG</strong><br>
+    Barangay Secretary</p>
+
+    <br>
+
+    <p><strong>RACHEL GRACE S. VILLAVER</strong><br>
+    Barangay Treasurer</p>
    
-    <h2 style="text-align:center;  text-decoration:">BARANGAY CLEARANCE</h2>
+      <p style="font-family:'Bookman Old Style; font-size:8pt; margin-top: 100px;" >NOT VALID WITHOUT SEAL</p>
 
-    <p><strong>TO WHOM IT MAY CONCERN:</strong></p>
+</div>
 
-    <p>
-        This is to certify that Mr./Ms./Mrs. _______________________________, 
-        ______ years old, ____________________, a native of __________________________, 
+
+ <div style="margin-left:230px; margin-right:35px;">
+    <h2 style="text-align:center;  margin-top: -0px;">BARANGAY CLEARANCE</h2>
+
+    <p style="font-family:'Times New Roman', serif; font-size:14pt;">TO WHOM IT MAY CONCERN:</p>
+
+    <p style="font-size:12pt;">
+        This is to certify that Mr./Ms./Mrs. {{$barangay_clearance->requestor->first_name}} {{$barangay_clearance->requestor->middle_initial}}. {{$barangay_clearance->requestor->last_name}},
+        {{ \Carbon\Carbon::parse($barangay_clearance->requestor->birthdate)->age }} years old, {{$barangay_clearance->native}}, a native of __________________________, 
         and a resident of Zone/Purok ______________________________, Ubaldo D. Laya, Iligan City.
     </p>
 
-    <p>
+    <p style="font-size:12pt;">
         This is to certify further that the above-mentioned person is known by this office and has the following:
     </p>
 
-    <p>Remarks:</p>
-    <ul>
-        <li>( ) No Derogatory Record and is of Good Moral Character</li>
-        <li>( ) Have Case Filed but Settled</li>
-        <li>( ) Have Pending Case FILED</li>
-        <li>( ) Have Case Filed Withdrawn/Dismissed</li>
+    <p style="font-size:12pt;">Remarks:</p>
+    <ul style="font-size:12pt;">
+        <li style="font-size:12pt;">( ) No Derogatory Record and is of Good Moral Character</li>
+        <li style="font-size:12pt;">( ) Have Case Filed but Settled</li>
+        <li style="font-size:12pt;">( ) Have Pending Case FILED</li>
+        <li style="font-size:12pt;">( ) Have Case Filed Withdrawn/Dismissed</li>
     </ul>
 
-    <p>Purpose: _______________________________</p>
-    <p>Date Issued: ___________________________</p>
+    <p style="font-size:12pt;">Purpose: _______________________________</p>
+    <p style="font-size:12pt;">Date Issued: ___________________________</p>
 
-    <div class="signature">
+    <div class="signature" style="font-size:12pt;">
         <p>__________________________</p>
         <p>Signature above Printed Name</p>
         <br><br>
        
     </div>
-  <div style="margin-left:250px; margin-right:15px;">
-  
-    <p style="margin:0;"><strong>HON. ANTONIO P. FLORES, CSSP</strong></p>
-    <p style="margin:0;">PUNONG BARANGAY</p>
+  <div style="margin-left:150px; margin-right:15px;" >
+ <p style="margin:0; border-bottom:1px solid black; display:inline-block;" style="font-size:14pt;">
+    <strong >HON. ANTONIO P. FLORES, CSSP</strong>
+</p>
+<p style="margin:0;" style="font-size:12pt;">PUNONG BARANGAY</p>
+
 
 </div><br><br><br><br>
 
